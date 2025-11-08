4d9a0430418e9fb5600059df44b035c8ba0b962c
elkuku <der.el.kuku@gmail.com>
2025-11-08 10:13:56 -0500

    First import

--- src/Main.ts
+++ src/Main.ts
@@ -1,21 +1,21 @@
-import * as Plugin from "iitcpluginkit";
+import * as Plugin from 'iitcpluginkit'
 
+import * as Handlebars from 'handlebars'
+import {HelperDelegate} from 'handlebars'
 
 class IitcHelperHandlebars implements Plugin.Class {
 
     init() {
-        console.log("IitcHelperHandlebars " + VERSION);
-
-        
-
-        // FILL ME
+        console.log(`HelperHandlebars ;) ${VERSION}`);
     }
 
+    public compile(template: string): HandlebarsTemplateDelegate {
+        return Handlebars.compile(template)
+    }
+
+    public registerHelper(name:string, function_: HelperDelegate) {
+        Handlebars.registerHelper(name, function_)
+    }
 }
 
-/**
- * use "main" to access you main class from everywhere
- * (same as window.plugin.IitcHelperHandlebars)
- */
-export const main = new IitcHelperHandlebars();
-Plugin.Register(main, "IitcHelperHandlebars");
+Plugin.Register(new IitcHelperHandlebars(), 'HelperHandlebars')
